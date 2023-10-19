41334f41e0ae682873baec96640c9e812c01c4c6
Anthony Taylor <ant.taylor2@gmail.com>
2023-10-19 00:29:03 -0400

    chore: adding setup configs for testing

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,30 @@
 module.exports = {
-    extends: ['universe', 'universe/shared/typescript-analysis', 'plugin:react-hooks/recommended'],
-    overrides: [
-      {
-        files: ['*.ts', '*.tsx', '*.d.ts'],
-        parserOptions: {
-          project: './tsconfig.json'
-        }
+  extends: [
+    'universe',
+    'universe/shared/typescript-analysis',
+    'plugin:react-hooks/recommended',
+    'plugin:testing-library/react',
+    'plugin:jest-dom/recommended'
+  ],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx', '*.d.ts'],
+      parserOptions: {
+        project: './tsconfig.json'
       }
-    ],
-    settings: {
-      'import/resolver': {
-        typescript: {} // this loads <rootdir>/tsconfig.json to ESLint
-      }
-    },
-    /* for lint-staged */
-    globals: {
-      __dirname: true
-    },
-    rules: {
-      'no-console': 'error'
     }
-  }
+  ],
+  settings: {
+    'import/resolver': {
+      typescript: {} // this loads <rootdir>/tsconfig.json to ESLint
+    }
+  },
+  /* for lint-staged */
+  globals: {
+    __dirname: true
+  },
+  rules: {
+    'no-console': 'error'
+  },
+  plugins: ['jest', 'testing-library']
+}
