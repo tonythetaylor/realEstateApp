da577d23885fd16ad512e2b3321f75124d847130
Anthony Taylor <ant.taylor2@gmail.com>
2023-10-19 08:53:24 -0400

    chore: adding navigation structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,117 @@
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { Button, StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 
-export default function App() {
+function HomeScreen({ navigation, route }) {
+  const [post, setPost] = useState()
+  useEffect(() => {
+    if (route.params?.post) {
+      // Post updated, do something with `route.params.post`
+      // For example, send the post to the server
+      setPost(route.params?.post)
+    }
+  }, [route.params?.post])
+
   return (
-    <SafeAreaProvider>
-      <View style={styles.container}>
-        <Text>Open up App.tsx to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </View>
-    </SafeAreaProvider>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button title="Create post" onPress={() => navigation.navigate('CreatePost')} />
+      <TouchableOpacity
+        onPress={() => {
+          // Pass and merge params back to home screen
+          navigation.navigate({
+            name: 'PostScreen',
+            params: { otherParam: post },
+            merge: true
+          })
+        }}
+      >
+        <Text>{route.params?.post}</Text>
+      </TouchableOpacity>
+    </View>
   )
 }
+
+function CreatePostScreen({ navigation, route }) {
+  const [postText, setPostText] = React.useState('')
+
+  return (
+    <>
+      <TextInput
+        multiline
+        placeholder="What's on your mind?"
+        style={{ height: 200, padding: 10, backgroundColor: 'white' }}
+        value={postText}
+        onChangeText={setPostText}
+      />
+      <Button
+        title="Done"
+        onPress={() => {
+          // Pass and merge params back to home screen
+          navigation.navigate({
+            name: 'Home',
+            params: { post: postText },
+            merge: true
+          })
+        }}
+      />
+    </>
+  )
+}
+
+function DetailsScreen({ route, navigation }) {
+  /* 2. Get the param */
+  const { itemId, otherParam } = route.params
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+      <Text>itemId: {JSON.stringify(itemId)}</Text>
+      <Text>otherParam: {JSON.stringify(otherParam)}</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() =>
+          navigation.push('Details', {
+            itemId: Math.floor(Math.random() * 100)
+          })
+        }
+      />
+      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+    </View>
+  )
+}
+
+function PostScreen({ route, navigation }) {
+  /* 2. Get the param */
+  const { itemId, otherParam } = route.params
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Post Screen</Text>
+      <Text>Post: {JSON.stringify(otherParam)}</Text>
+      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+    </View>
+  )
+}
+
+const Stack = createNativeStackNavigator()
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Home' }} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+        <Stack.Screen name="CreatePost" component={CreatePostScreen} />
+        <Stack.Screen name="PostScreen" component={PostScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+
+export default App
 
 const styles = StyleSheet.create({
   container: {
